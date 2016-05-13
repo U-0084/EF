74e128b55ab88d3a3c565ce0998169e7a03fe3f2
大橋　勇 <y_ohashi@waku-2.com>
2016-05-13 20:05:22 +0900

    classの時代がきた。

--- public/javascripts/app/main.js
+++ public/javascripts/app/main.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 enchant();
 
 var screen_width = 640;
@@ -33,6 +37,7 @@ window.onload = function () {
 	var game = new Game(screen_width, screen_height);
 	game.preload(assets);
 	game.fps = 15;
+	game.keybind(32, 'space');
 	game.onload = function () {
 
 		var LifeP1 = new Entity();
@@ -49,88 +54,25 @@ window.onload = function () {
 		LifeP2.y = 10;
 		LifeP2.backgroundColor = '#27e4b2';
 
-		var player01 = new Avatar("2:2:1:2004:21230:22480");
-		var input = game.input;
-		player01.x = screen_width / 3;
-		player01.y = 220;
-		player01.action = 'attack';
-		if (input.left) {
-			player01.x -= player_speed;
-		}
-		if (input.right) {
-			player01.x += player_speed;
-		}
-		if (input.up) {
-			player01.y -= player_speed;
-		}
-		if (input.down) {
-			player01.y += player_speed;
-		}
+		var Player01 = function () {
+			_createClass(Player01, [{
+				key: 'initialize',
+				value: function initialize() {
+					var player01 = new Avatar("2:2:1:2004:21230:22480");
+					game.rootScene.addChild(player01);
+					player01.x = screen_width / 3;
+					player01.y = 220;
+				}
+			}]);
 
-		var Player02 = Class.create(Sprite, {
-			initialize: function initialize(x, y) {
-				Sprite.call(this, 160, 160);
-				this.image = game.assets[player02_image];
-				this.x = x;
-				this.y = y;
-				this.frame = 3;
-			},
-			onenterframe: function onenterframe() {
-				var player_speed = 30;
-				var top = 0;
-				var left = 0;
-				var right = screen_width - this.width;
-				var bottom = screen_height - this.height;
+			function Player01(attack) {
+				_classCallCheck(this, Player01);
 
+				this.action = 'attack';
+			}
 
-				if (gamepad) {
-					if (gamepad.axes[0] < -0.5) {
-						this.x -= player_speed;
-						this.frame = this.age % 3 + 9;
-					}
-					if (gamepad.axes[0] > 0.5) {
-						this.x += player_speed;
-						this.frame = this.age % 3 + 18;
-					}
-					if (gamepad.axes[1] < -0.5) {
-						this.y -= player_speed;
-						this.frame = this.age % 3 + 27;
-					}
-					if (gamepad.axes[1] > 0.5) {
-						this.y += player_speed;
-						this.frame = this.age % 3;
-					}
-				}
-				if (input.left) {
-					this.x -= player_speed;
-					this.frame = this.age % 3 + 2;
-				}
-				if (input.right) {
-					this.x += player_speed;
-					this.frame = this.age % 3 + 2;
-				}
-				if (input.up) {
-					this.y -= player_speed;
-					this.frame = this.age % 3 + 27;
-				}
-				if (input.down) {
-					this.y += player_speed;
-					this.frame = 7;
-				}
-
-				if (this.x < left) {
-					this.x = left;
-				} else if (this.x > right) {
-					this.x = right;
-				}
-				if (this.y < top) {
-					this.y = top;
-				} else if (this.y > bottom) {
-					this.y = bottom;
-				}
-			},
-			attack: function attack() {}
-		});
+			return Player01;
+		}();
 
 		game.rootScene.on('enterframe', function (topScene) {
 
@@ -156,26 +98,112 @@ window.onload = function () {
 				lifeGaugeGroup.addChild(LifeP2);
 				rootScene.addChild(lifeGaugeGroup);
 
-				rootScene.addChild(player01);
+				// rootScene.addChild(player01);
 
-				var player02 = new Player02(screen_width / 1.5, 130);
-				rootScene.addChild(player02);
+				// const player02 = new Player02(screen_width / 1.5, 130);
+				// rootScene.addChild(player02);
 
-				if (player01.x > player02.x) {
-					player01.left();
-				} else {
-					player01.right();
-				}
+				// if (player01.x > player02.x) {
+				// 	player01.left();
+				// } else {
+				// 	player01.right();
+				// }
+
+				var player01 = new Player01();
+				player01.initialize();
+
+				console.log(player01.initialize);
 
 				return scene;
 			}
 
 			topScene();
 
-			if (game.input.up) {
+			if (game.input.space) {
 				battleScene();
 			}
 		});
 	};
 	game.start();
 };
+
+// player01.x = screen_width / 3;
+// player01.y = 220;
+// const player_speed = 30;
+// if (game.input.left) {
+// 	player01.x -= player_speed;
+// 	player01.action = 'attack';
+// }
+// if (game.input.right) {
+// 	player01.x += player_speed;
+// }
+// if (game.input.up) {
+// 	player01.y -= player_speed;
+// }
+// if (game.input.down) {
+// 	player01.y += player_speed;
+// }
+
+// const Player02 = Class.create(Sprite, {
+// 	initialize: function(x, y) {
+// 		Sprite.call(this, 160, 160);
+// 		this.image = game.assets[player02_image];
+// 		this.x = x;
+// 		this.y = y;
+// 		this.frame = 3;
+// 	},
+// 	onenterframe: function() {
+// 		const player_speed = 30;
+// 		const [top, left] = [0, 0];
+// 		const [right, bottom] = [screen_width - this.width, screen_height - this.height];
+
+// 		if (gamepad) {
+// 			if (gamepad.axes[0] < -0.5) {
+// 				this.x -= player_speed;
+// 				this.frame = this.age % 3 + 9;
+// 			}
+// 			if (gamepad.axes[0] > 0.5) {
+// 				this.x += player_speed;
+// 				this.frame = this.age % 3 + 18;
+// 			}
+// 			if (gamepad.axes[1] < -0.5) {
+// 				this.y -= player_speed;
+// 				this.frame = this.age % 3 + 27;
+// 			}
+// 			if (gamepad.axes[1] > 0.5) {
+// 				this.y += player_speed;
+// 				this.frame = this.age % 3;
+// 			}
+// 		}
+// 		if (input.left) {
+// 			this.x -= player_speed;
+// 			this.frame = this.age % 3 + 2;
+// 		}
+// 		if (input.right) {
+// 			this.x += player_speed;
+// 			this.frame = this.age % 3 + 2;
+// 		}
+// 		if (input.up) {
+// 			this.y -= player_speed;
+// 			this.frame = this.age % 3 + 27;
+// 		}
+// 		if (input.down) {
+// 			this.y += player_speed;
+// 			this.frame = 7;
+// 		}
+
+// 		if (this.x < left) {
+// 			this.x = left;
+// 		} else if (this.x > right) {
+// 			this.x = right;
+// 		}
+// 		if (this.y < top) {
+// 			this.y = top;
+// 		} else if (this.y > bottom) {
+// 			this.y = bottom;
+// 		}
+// 	},
+// 	attack: function() {
+
+// 	}
+// });
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -38,6 +38,7 @@ window.onload = () => {
 	const game = new Game(screen_width, screen_height);
 	game.preload(assets);
 	game.fps = 15;
+	game.keybind(32, 'space');
 	game.onload = () => {
 
 		const LifeP1 = new Entity();
@@ -55,87 +56,17 @@ window.onload = () => {
 		LifeP2.backgroundColor = '#27e4b2';
 
 
-		const player01 = new Avatar("2:2:1:2004:21230:22480");
-		const input = game.input;
-		player01.x = screen_width / 3;
-		player01.y = 220;
-		player01.action = 'attack';
-		if (input.left) {
-			player01.x -= player_speed;
-		}
-		if (input.right) {
-			player01.x += player_speed;
-		}
-		if (input.up) {
-			player01.y -= player_speed;
-		}
-		if (input.down) {
-			player01.y += player_speed;
-		}
-
-		const Player02 = Class.create(Sprite, {
-			initialize: function(x, y) {
-				Sprite.call(this, 160, 160);
-				this.image = game.assets[player02_image];
-				this.x = x;
-				this.y = y;
-				this.frame = 3;
-			},
-			onenterframe: function() {
-				const player_speed = 30;
-				const [top, left] = [0, 0];
-				const [right, bottom] = [screen_width - this.width, screen_height - this.height];
-
-				if (gamepad) {
-					if (gamepad.axes[0] < -0.5) {
-						this.x -= player_speed;
-						this.frame = this.age % 3 + 9;
-					}
-					if (gamepad.axes[0] > 0.5) {
-						this.x += player_speed;
-						this.frame = this.age % 3 + 18;
-					}
-					if (gamepad.axes[1] < -0.5) {
-						this.y -= player_speed;
-						this.frame = this.age % 3 + 27;
-					}
-					if (gamepad.axes[1] > 0.5) {
-						this.y += player_speed;
-						this.frame = this.age % 3;
-					}
-				}
-				if (input.left) {
-					this.x -= player_speed;
-					this.frame = this.age % 3 + 2;
-				}
-				if (input.right) {
-					this.x += player_speed;
-					this.frame = this.age % 3 + 2;
-				}
-				if (input.up) {
-					this.y -= player_speed;
-					this.frame = this.age % 3 + 27;
-				}
-				if (input.down) {
-					this.y += player_speed;
-					this.frame = 7;
-				}
-
-				if (this.x < left) {
-					this.x = left;
-				} else if (this.x > right) {
-					this.x = right;
-				}
-				if (this.y < top) {
-					this.y = top;
-				} else if (this.y > bottom) {
-					this.y = bottom;
-				}
-			},
-			attack: function() {
-
+		class Player01 {
+			initialize() {
+				const player01 = new Avatar("2:2:1:2004:21230:22480");
+				game.rootScene.addChild(player01);
+				player01.x = screen_width / 3;
+				player01.y = 220;
 			}
-		});
+			constructor(attack) {
+				this.action = 'attack';
+			}
+		}
 
 		game.rootScene.on('enterframe', (topScene) => {
 
@@ -157,32 +88,117 @@ window.onload = () => {
 				bg.y = 0;
 				rootScene.addChild(bg);
 
-
 				const lifeGaugeGroup = new Group();
 				lifeGaugeGroup.addChild(LifeP1);
 				lifeGaugeGroup.addChild(LifeP2);
 				rootScene.addChild(lifeGaugeGroup);
 
-				rootScene.addChild(player01);
+				// rootScene.addChild(player01);
 
-				const player02 = new Player02(screen_width / 1.5, 130);
-				rootScene.addChild(player02);
+				// const player02 = new Player02(screen_width / 1.5, 130);
+				// rootScene.addChild(player02);
 
-				if (player01.x > player02.x) {
-					player01.left();
-				} else {
-					player01.right();
-				}
+				// if (player01.x > player02.x) {
+				// 	player01.left();
+				// } else {
+				// 	player01.right();
+				// }
+
+				const player01 = new Player01();
+				player01.initialize();
+
+				console.log(player01.initialize);
 
 				return scene;
 			}
 
 			topScene();
 
-			if (game.input.up) {
+			if (game.input.space) {
 				battleScene();
 			}
 		});
 	};
 	game.start();
 };
+
+// player01.x = screen_width / 3;
+		// player01.y = 220;
+		// const player_speed = 30;
+		// if (game.input.left) {
+		// 	player01.x -= player_speed;
+		// 	player01.action = 'attack';
+		// }
+		// if (game.input.right) {
+		// 	player01.x += player_speed;
+		// }
+		// if (game.input.up) {
+		// 	player01.y -= player_speed;
+		// }
+		// if (game.input.down) {
+		// 	player01.y += player_speed;
+		// }
+
+		// const Player02 = Class.create(Sprite, {
+		// 	initialize: function(x, y) {
+		// 		Sprite.call(this, 160, 160);
+		// 		this.image = game.assets[player02_image];
+		// 		this.x = x;
+		// 		this.y = y;
+		// 		this.frame = 3;
+		// 	},
+		// 	onenterframe: function() {
+		// 		const player_speed = 30;
+		// 		const [top, left] = [0, 0];
+		// 		const [right, bottom] = [screen_width - this.width, screen_height - this.height];
+
+		// 		if (gamepad) {
+		// 			if (gamepad.axes[0] < -0.5) {
+		// 				this.x -= player_speed;
+		// 				this.frame = this.age % 3 + 9;
+		// 			}
+		// 			if (gamepad.axes[0] > 0.5) {
+		// 				this.x += player_speed;
+		// 				this.frame = this.age % 3 + 18;
+		// 			}
+		// 			if (gamepad.axes[1] < -0.5) {
+		// 				this.y -= player_speed;
+		// 				this.frame = this.age % 3 + 27;
+		// 			}
+		// 			if (gamepad.axes[1] > 0.5) {
+		// 				this.y += player_speed;
+		// 				this.frame = this.age % 3;
+		// 			}
+		// 		}
+		// 		if (input.left) {
+		// 			this.x -= player_speed;
+		// 			this.frame = this.age % 3 + 2;
+		// 		}
+		// 		if (input.right) {
+		// 			this.x += player_speed;
+		// 			this.frame = this.age % 3 + 2;
+		// 		}
+		// 		if (input.up) {
+		// 			this.y -= player_speed;
+		// 			this.frame = this.age % 3 + 27;
+		// 		}
+		// 		if (input.down) {
+		// 			this.y += player_speed;
+		// 			this.frame = 7;
+		// 		}
+
+		// 		if (this.x < left) {
+		// 			this.x = left;
+		// 		} else if (this.x > right) {
+		// 			this.x = right;
+		// 		}
+		// 		if (this.y < top) {
+		// 			this.y = top;
+		// 		} else if (this.y > bottom) {
+		// 			this.y = bottom;
+		// 		}
+		// 	},
+		// 	attack: function() {
+
+		// 	}
+		// });
